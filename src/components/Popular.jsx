2381e09d6b3be0d2979edb5b6b6153449873e1f5
Kyle Wong <you@example.com>
2023-08-18 15:29:54 -0400

    added carousel feature with splide

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,4 +1,7 @@
 import { useEffect, useState } from "react";
+import styled from 'styled-components';
+import {Splide, SplideSlide} from '@splidejs/react-splide';
+import '@splidejs/splide/dist/css/splide.min.css';
 
 function Popular() {
 
@@ -12,13 +15,43 @@ function Popular() {
     const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
     const data = await api.json();
     setPopular(data.recipes)
-    console.log(data)
   }
+
   return (
     <div>
-        Popular
+      <Wrapper>
+        <h3>Popular picks</h3>
+
+        <Splide>
+          {popular.map((recipe) => {
+            return(
+              <SplideSlide>
+                <Card key={recipe.id}>
+                  <p>{recipe.title}</p>
+                  <img src={recipe.image} alt={recipe.title} />
+                </Card>
+              </SplideSlide>
+            );
+          })}
+        </Splide>
+      </Wrapper>
     </div>
   )
 }
+
+const Wrapper = styled.div`
+  margin: 4rem 0rem;
+`
+
+const Card = styled.div`
+  min-height: 25rem;
+  border-radius: 2rem;
+  overflow: hidden;
+
+  img{
+    border-radius: 2rem;
+  }
+`
+
 
 export default Popular
