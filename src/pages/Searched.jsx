58a9e5108a823fd6cb8beb31b7932b7c7b65df37
Kyle Wong <you@example.com>
2023-09-05 12:31:56 -0400

    added link function to all pages, and motion trnasition between pages

--- src/pages/Searched.jsx
+++ src/pages/Searched.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 function Searched() {
 	const [searchedRecipes, setSearchedRecipes] = useState([]);
@@ -21,7 +22,11 @@ function Searched() {
 	}, [params.search]);
 
 	return (
-		<Grid>
+		<Grid
+			animate={{ opacity: 1 }}
+			initial={{ opacity: 0 }}
+			exit={{ opacity: 0 }}
+			transition={{ duration: 0.5 }}>
 			{searchedRecipes.map((item) => {
 				return (
 					<Card key={item.id}>
@@ -36,7 +41,7 @@ function Searched() {
 	);
 }
 
-const Grid = styled.div`
+const Grid = styled(motion.div)`
 	display: grid;
 	grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
 	grid-gap: 3rem;
